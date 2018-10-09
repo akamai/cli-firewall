958f5dcb9f33250ac75bca8f96f6230ae681da53
Vreddhi Bhat <vreddhi.bhat@gmail.com>
2018-10-09 11:57:50 +0530

    listCidr and access fix

--- bin/firewallruleswrapper.py
+++ bin/firewallruleswrapper.py
@@ -14,8 +14,13 @@ import json
 
 
 class fireShield(object):
-    def __init__(self, access_hostname):
+    def __init__(self, access_hostname,account_switch_key):
         self.access_hostname = access_hostname
+        self.access_hostname = access_hostname
+        if account_switch_key != '':
+            self.account_switch_key = '&accountSwitchKey=' + account_switch_key
+        else:
+            self.account_switch_key = ''
 
     def list_maps(self, session):
         """
@@ -32,10 +37,17 @@ class fireShield(object):
             (list_mapsRespose) Object with all details
         """
 
-        list_maps_url = 'https://' + self.access_hostname + \
-            '/siteshield/v1/maps'
-        list_maps_response = session.get(
-            list_maps_url)
+        list_maps_url = 'https://' + self.access_hostname + '/siteshield/v1/maps'
+
+        #This is to ensure accountSwitchKey works for internal users
+        if '?' in list_maps_url:
+            list_maps_url = list_maps_url + self.account_switch_key
+        else:
+            #Replace & with ? if there is no query string in URL
+            self.account_switch_key = self.account_switch_key.translate(self.account_switch_key.maketrans('&','?'))
+            list_maps_url = list_maps_url + self.account_switch_key
+
+        list_maps_response = session.get(list_maps_url)
         return list_maps_response
 
     def get_map(self, session, mapId):
@@ -53,8 +65,16 @@ class fireShield(object):
             (get_mapRespose) Object with all details
         """
 
-        get_map_url = 'https://' + self.access_hostname + \
-            '/siteshield/v1/maps/' + str(mapId)
+        get_map_url = 'https://' + self.access_hostname + '/siteshield/v1/maps/' + str(mapId)
+
+        #This is to ensure accountSwitchKey works for internal users
+        if '?' in get_map_url:
+            get_map_url = get_map_url + self.account_switch_key
+        else:
+            #Replace & with ? if there is no query string in URL
+            self.account_switch_key = self.account_switch_key.translate(self.account_switch_key.maketrans('&','?'))
+            get_map_url = get_map_url + self.account_switch_key
+
         get_map_response = session.get(get_map_url)
         return get_map_response
 
@@ -75,8 +95,16 @@ class fireShield(object):
 
         acknowledge_map_url = 'https://' + self.access_hostname + \
             '/siteshield/v1/maps/' + str(mapId) + '/acknowledge'
-        acknowledge_map_response = session.post(
-            acknowledge_map_url)
+
+        #This is to ensure accountSwitchKey works for internal users
+        if '?' in acknowledge_map_url:
+            acknowledge_map_url = acknowledge_map_url + self.account_switch_key
+        else:
+            #Replace & with ? if there is no query string in URL
+            self.account_switch_key = self.account_switch_key.translate(self.account_switch_key.maketrans('&','?'))
+            acknowledge_map_url = acknowledge_map_url + self.account_switch_key
+
+        acknowledge_map_response = session.post(acknowledge_map_url)
         return acknowledge_map_response
 
     def list_services(self, session):
@@ -96,8 +124,16 @@ class fireShield(object):
 
         list_services_url = 'https://' + self.access_hostname + \
             '/firewall-rules-manager/v1/services'
-        list_services_response = session.get(
-            list_services_url)
+
+        #This is to ensure accountSwitchKey works for internal users
+        if '?' in list_services_url:
+            list_services_url = list_services_url + self.account_switch_key
+        else:
+            #Replace & with ? if there is no query string in URL
+            self.account_switch_key = self.account_switch_key.translate(self.account_switch_key.maketrans('&','?'))
+            list_services_url = list_services_url + self.account_switch_key
+
+        list_services_response = session.get(list_services_url)
         return list_services_response
 
     def get_service(self, session, serviceId):
@@ -117,8 +153,16 @@ class fireShield(object):
 
         get_service_url = 'https://' + self.access_hostname + \
             '/firewall-rules-manager/v1/services/' + str(serviceId)
-        get_service_response = session.get(
-            get_service_url)
+
+        #This is to ensure accountSwitchKey works for internal users
+        if '?' in get_service_url:
+            get_service_url = get_service_url + self.account_switch_key
+        else:
+            #Replace & with ? if there is no query string in URL
+            self.account_switch_key = self.account_switch_key.translate(self.account_switch_key.maketrans('&','?'))
+            get_service_url = get_service_url + self.account_switch_key
+
+        get_service_response = session.get(get_service_url)
         return get_service_response
 
     def list_cidr(self, session):
@@ -138,8 +182,16 @@ class fireShield(object):
 
         list_cidr_url = 'https://' + self.access_hostname + \
             '/firewall-rules-manager/v1/cidr-blocks'
-        list_cidr_response = session.get(
-            list_cidr_url)
+
+        #This is to ensure accountSwitchKey works for internal users
+        if '?' in list_cidr_url:
+            list_cidr_url = list_cidr_url + self.account_switch_key
+        else:
+            #Replace & with ? if there is no query string in URL
+            self.account_switch_key = self.account_switch_key.translate(self.account_switch_key.maketrans('&','?'))
+            list_cidr_url = list_cidr_url + self.account_switch_key
+
+        list_cidr_response = session.get(list_cidr_url)
         return list_cidr_response
 
     def list_subscriptions(self, session):
@@ -159,8 +211,16 @@ class fireShield(object):
 
         listSubscriptions_url = 'https://' + self.access_hostname + \
             '/firewall-rules-manager/v1/subscriptions'
-        listSubscriptions_response = session.get(
-            listSubscriptions_url)
+
+        #This is to ensure accountSwitchKey works for internal users
+        if '?' in listSubscriptions_url:
+            listSubscriptions_url = listSubscriptions_url + self.account_switch_key
+        else:
+            #Replace & with ? if there is no query string in URL
+            self.account_switch_key = self.account_switch_key.translate(self.account_switch_key.maketrans('&','?'))
+            listSubscriptions_url = listSubscriptions_url + self.account_switch_key
+
+        listSubscriptions_response = session.get(listSubscriptions_url)
         return listSubscriptions_response
 
     def update_subscriptions(self, session, subscriptionData):
@@ -180,8 +240,16 @@ class fireShield(object):
 
         update_subscriptions_url = 'https://' + self.access_hostname + \
             '/firewall-rules-manager/v1/subscriptions'
-        update_subscriptions_response = session.put(
-            update_subscriptions_url, data=subscriptionData)
+
+        #This is to ensure accountSwitchKey works for internal users
+        if '?' in update_subscriptions_url:
+            update_subscriptions_url = update_subscriptions_url + self.account_switch_key
+        else:
+            #Replace & with ? if there is no query string in URL
+            self.account_switch_key = self.account_switch_key.translate(self.account_switch_key.maketrans('&','?'))
+            update_subscriptions_url = update_subscriptions_url + self.account_switch_key
+
+        update_subscriptions_response = session.put(update_subscriptions_url, data=subscriptionData)
         return update_subscriptions_response
 
     def acknowledge_map(self, session, mapId):
@@ -201,6 +269,14 @@ class fireShield(object):
 
         acknowledge_map_url = 'https://' + self.access_hostname + \
             '/siteshield/v1/maps/' + str(mapId) + '/acknowledge'
-        acknowledge_map_response = session.post(
-            acknowledge_map_url)
+
+        #This is to ensure accountSwitchKey works for internal users
+        if '?' in acknowledge_map_url:
+            acknowledge_map_url = acknowledge_map_url + self.account_switch_key
+        else:
+            #Replace & with ? if there is no query string in URL
+            self.account_switch_key = self.account_switch_key.translate(self.account_switch_key.maketrans('&','?'))
+            acknowledge_map_url = acknowledge_map_url + self.account_switch_key
+
+        acknowledge_map_response = session.post(acknowledge_map_url)
         return acknowledge_map_response
